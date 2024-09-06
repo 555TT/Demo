74ea07316e9e8b9d8b1982f32cf818d09cf77518
whr <15238530210@163.com>
2024-09-06 19:06:17 +0800

    多线程交替打印

---
+++ other/src/main/java/other/交替打印/ReentrantLockPrint.java
@@ -0,0 +1,61 @@
+package other.交替打印;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author: 小手WA凉
+ * @create: 2024-08-21
+ */
+public class ReentrantLockPrint {
+    static final ReentrantLock lock = new ReentrantLock();
+    static final Condition condition1 = lock.newCondition();
+    static final Condition condition2 = lock.newCondition();
+    static boolean flag = true;//true 打印1 false 打印2
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    try {
+                        lock.lock();
+                        if (!flag)
+                            condition1.await();
+                        System.out.print("1 ");
+                        flag = false;
+                        condition2.signal();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    } finally {
+                        lock.unlock();
+                    }
+                }
+
+            }
+        }
+        );
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    try {
+                        lock.lock();
+                        if (flag)
+                            condition2.await();
+                        System.out.print("2 ");
+                        flag = true;
+                        condition1.signal();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    } finally {
+                        lock.unlock();
+                    }
+                }
+            }
+        }
+        );
+        thread1.start();
+        thread2.start();
+    }
+}
---
+++ other/src/main/java/other/交替打印/SynPrint.java
@@ -0,0 +1,46 @@
+package other.交替打印;
+
+/**
+ * @author: 小手WA凉
+ * @create: 2024-08-21
+ */
+public class SynPrint {
+
+    static final Object lock = new Object();
+    static int state = 1;
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i <100; i++) {
+                synchronized (lock) {
+                    try {
+                        if (state % 2 != 0)
+                            lock.wait();
+                        System.out.print("1 ");
+                        state++;
+                        lock.notify();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                synchronized (lock) {
+                    try {
+                        if (state % 2 == 0)
+                            lock.wait();
+                        System.out.print("2 ");
+                        state++;
+                        lock.notify();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+        thread2.start();
+        thread1.start();
+    }
+}
