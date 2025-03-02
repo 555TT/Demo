9ec70a5e2cca5b6941827987c177d98af4e7639e
whr <15238530210@163.com>
2025-03-02 20:49:39 +0800

    快速排序改进

--- algorithm/src/排序算法/QuickSort.java
+++ algorithm/src/排序算法/QuickSort.java
@@ -2,6 +2,7 @@ package 排序算法;
 
 /**
  * 快速排序
+ * 注意点：while循环中线从右往左找；一定要先将基准元素交换到最左边，然后while退出后再将基准元素交换到正确位置
  *
  * @author: wanghaoran1
  * @create: 2025-02-26
@@ -11,33 +12,40 @@ public class QuickSort {
         if (left >= right) {
             return;
         }
+        // 随机选择基准元素
+        int pivotIndex = left + (int) (Math.random() * (right - left + 1));
+        int pivot = arr[pivotIndex];
+        // 将基准元素交换到最左边
+        swap(arr, left, pivotIndex);
         int i = left;//左边界
         int j = right;//右边界
-        int x = arr[i];//基准元素，这里选择左边界元素作为基准元素，用x暂存基准元素
         while (i < j) {
             // 从右向左寻找第一个小于基准元素的元素
-            while (i < j && arr[j] >= x) {//！！一定要先从右向左找
+            while (i < j && arr[j] >= pivot) {//！！一定要先从右向左找
                 j--;
             }
             // 从左向右寻找第一个大于基准元素的元素
-            while (i < j && arr[i] <= x) {
+            while (i < j && arr[i] <= pivot) {
                 i++;
             }
             if (i < j) {//如果找到了，交换两边的值
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                swap(arr, i, j);
             }
         }
         // 将基准元素放到正确的位置
-        arr[left] = arr[i];//arr[i]也是小于基准元素的值，将它放到最左边
-        arr[i] = x;//将基准元素放到i这个中间位置
+        swap(arr, left, i);
         quickSort(arr, left, i - 1);
         quickSort(arr, i + 1, right);
     }
 
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
     public static void main(String[] args) {
-        int[] arr = {30, 40, 60, 10, 20, 50};
+        int[] arr = {30, 40, 60, 10, 20, 50, 60};
         quickSort(arr, 0, arr.length - 1);
         for (int num : arr) {
             System.out.printf("%d ", num);
